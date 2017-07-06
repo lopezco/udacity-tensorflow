7f9adf9ca9ff00d0f6969171a473e9e1fd2789e1
Jose LOPEZ <jose.lopez@metronlab.com>
2017-07-06 10:17:39 +0200

    Rename environment variables to show associated service

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Read environment
+export PASSWORD=${JUPYTER_PASSWORD:-admin}
+export PORT=${JUPYTER_PORT:-8888}
+export TENSORBOARD_LOGDIR=${TENSORBOARD_LOGDIR:-./logs}
+export TENSORBOARD_PORT=${TENSORBOARD_PORT:-6006}
+
 # Launch tensorboard
-tensorboard --logdir ./logs --host 0.0.0.0 --port 6006 &
+tensorboard --logdir ${TENSORBOARD_LOGDIR} --host 0.0.0.0 --port ${TENSORBOARD_PORT} &
 
 # Launch jupyter
 /run_jupyter.sh --allow-root
